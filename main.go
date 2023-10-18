959ed31e10134c49febd919eb0251409fda130dc
fitra.amrullah <fitra.amrullah@artajasa.co.id>
2023-10-18 07:55:42 +0700

    test direct simulation

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
+import "time"
+
 import "fmt"
 import "github.com/go-rod/rod"
 
+
 func main() {
     page := rod.New().MustConnect().MustPage("https://www.wikipedia.org/")
+    fmt.Println("Navigating to page")
     page.MustWaitStable()
-    page.Mouse.Scroll(0, -300, 10)
+    simulateScroll(page)
     fmt.Println(
         page.MustEval("() => document.title"),	
 	)
+}
+
+
+func simulateScroll(page *rod.Page) {
+	sleep := func() { time.Sleep(time.Millisecond * 200) }
+
+	for i := 0; i < 3; i++ {
+		page.Mouse.Scroll(0, -300, 10)
+		sleep()
+
+		page.Mouse.Scroll(0, 300, 10)
+		sleep()
+	}
 }

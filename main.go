f1bb7fdb25f0a81d170c03d2ec611e992ae0fdca
fitra.amrullah <fitra.amrullah@artajasa.co.id>
2023-10-19 07:18:45 +0700

    add proxy

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import "github.com/go-rod/rod"
 import "github.com/go-rod/rod/lib/launcher"
 
 func main() {
+	fmt.Println("Set proxy")
 	l := launcher.New().Set("proxy-server", "gw1.dataimpulse.com:10000")
 	controlURL, _ := l.Launch()
     browser := rod.New().ControlURL(controlURL).MustConnect()
-	
+	fmt.Println("Set login")
 	go browser.MustHandleAuth("1b4723160125bb95b27f", "aca43dd13647c321")()
 
 	browser.MustIgnoreCertErrors(true)

e2c4486c2de6957ece2d57b7bc0ea6b9841ddc2c
fitra.amrullah <fitra.amrullah@artajasa.co.id>
2023-10-20 03:54:15 +0700

    managed rod docker

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import "github.com/go-rod/rod"
 import "github.com/go-rod/rod/lib/launcher"
 
 func main() {
-	fmt.Println("Set proxy")
-	l := launcher.New().Set("proxy-server", "gw1.dataimpulse.com:10000")
-	controlURL, _ := l.Launch()
-    browser := rod.New().ControlURL(controlURL).MustConnect()
-	fmt.Println("Set login")
-	go browser.MustHandleAuth("1b4723160125bb95b27f", "aca43dd13647c321")()
+	// fmt.Println("Set proxy")
+	// l := launcher.New().Set("proxy-server", "gw1.dataimpulse.com:10000")
+	// controlURL, _ := l.Launch()
+    // browser := rod.New().ControlURL(controlURL).MustConnect()
+	// fmt.Println("Set login")
+	// go browser.MustHandleAuth("1b4723160125bb95b27f", "aca43dd13647c321")()
 
-	browser.MustIgnoreCertErrors(true)
+	// browser.MustIgnoreCertErrors(true)
+	l := launcher.MustNewManaged("")
+	l.Set("disable-gpu").Delete("disable-gpu")
+
+	l.Headless(false).XVFB("--server-num=5", "--server-args=-screen 0 1600x900x16")
+	browser := rod.New().Client(l.MustClient()).MustConnect()
 
 	page := browser.MustPage("https://www.wikipedia.org/")
     fmt.Println("Navigating to page")
